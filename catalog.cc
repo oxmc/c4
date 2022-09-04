ed1230c009b0bcb337a03803105ef4a8dbf71044
oxmc <67136658+oxmc@users.noreply.github.com>
2022-09-04 00:13:07 -0700

    Update catalog.cc

--- catalog.cc
+++ catalog.cc
@@ -1,10 +1,9 @@
 {
-    c4 = "https://raw.githubusercontent.com/oxmc/c4/main/c4.lua",
     hello = "https://raw.githubusercontent.com/oxmc/c4/main/deps/hello.lua",
 
     json = "https://raw.githubusercontent.com/coolacid/ComputerCraft/master/external/json.lua",
     base64 = "https://raw.githubusercontent.com/brooswit/c4-common/main/libs/base64.lua",
     localStorage = "https://raw.githubusercontent.com/brooswit/c4-common/main/libs/localStorage.lua",
     button = "https://raw.githubusercontent.com/Siarko/ButtonApi/master/button.lua",
-    ccbuttons = "https://raw.githubusercontent.com/oxmc/c4/main/deps/buttons.lua"
+    github = "https://pastebin.com/raw/p8PJVxC4"
 }
