99b88671ee92d6979c28fde07fbcf5d847c7cb67
oxmc <67136658+oxmc@users.noreply.github.com>
2022-09-03 23:58:59 -0700

    Update catalog.cc

--- catalog.cc
+++ catalog.cc
@@ -1,16 +1,9 @@
 {
     c4 = "https://raw.githubusercontent.com/oxmc/c4/main/c4.lua",
-    hello = "https://raw.githubusercontent.com/brooswit/c4/main/hello.lua",
+    hello = "https://raw.githubusercontent.com/oxmc/c4/main/deps/hello.lua",
 
     json = "https://raw.githubusercontent.com/coolacid/ComputerCraft/master/external/json.lua",
     base64 = "https://raw.githubusercontent.com/brooswit/c4-common/main/libs/base64.lua",
     localStorage = "https://raw.githubusercontent.com/brooswit/c4-common/main/libs/localStorage.lua",
-    buttons = "https://raw.githubusercontent.com/oxmc/c4/main/deps/buttons.lua",
-
-    LaunchDarkly = "https://raw.githubusercontent.com/brooswit/c4-launchdarkly/main/libs/LaunchDarkly.lua",
-
-    ld_config = "https://raw.githubusercontent.com/brooswit/c4-launchdarkly/main/apps/ld_config.lua",
-    ld_redstone_flag_trigger = "https://raw.githubusercontent.com/brooswit/c4-launchdarkly/main/apps/ld_redstone_flag_trigger.lua",
-    ld_redstone_track_item_count = "https://raw.githubusercontent.com/brooswit/c4-launchdarkly/main/apps/ld_redstone_track_item_count.lua",
-    ld_redstone_variation = "https://raw.githubusercontent.com/brooswit/c4-launchdarkly/main/apps/ld_redstone_variation.lua"
+    buttons = "https://raw.githubusercontent.com/oxmc/c4/main/deps/buttons.lua"
 }
