509af0177c0fd7e92a50c0e2c85fdfb4ccf59958
oxmc <67136658+oxmc@users.noreply.github.com>
2022-09-03 23:32:15 -0700

    Update catalog.cc

--- catalog.cc
+++ catalog.cc
@@ -1,10 +1,11 @@
 {
-    c4 = "https://raw.githubusercontent.com/brooswit/c4/main/c4.lua",
+    c4 = "https://raw.githubusercontent.com/oxmc/c4/main/c4.lua",
     hello = "https://raw.githubusercontent.com/brooswit/c4/main/hello.lua",
 
     json = "https://raw.githubusercontent.com/coolacid/ComputerCraft/master/external/json.lua",
     base64 = "https://raw.githubusercontent.com/brooswit/c4-common/main/libs/base64.lua",
     localStorage = "https://raw.githubusercontent.com/brooswit/c4-common/main/libs/localStorage.lua",
+    buttons = "https://raw.githubusercontent.com/oxmc/c4/main/deps/buttons.lua",
 
     LaunchDarkly = "https://raw.githubusercontent.com/brooswit/c4-launchdarkly/main/libs/LaunchDarkly.lua",
 
